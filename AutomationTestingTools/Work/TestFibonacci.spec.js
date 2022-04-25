ef0c7e82a88dce20f4c4d2c245a2fc45e1d26df4
ValdiviaCgznt <103444882+ValdiviaCgznt@users.noreply.github.com>
2022-04-25 12:54:39 -0500

    Add files via upload

---
+++ AutomationTestingTools/Work/TestFibonacci.spec.js
@@ -0,0 +1,14 @@
+const {fibonacci} = require("./Fibonacci");
+
+
+describe("TestFibonacci", () => {
+
+    it("returns fibonacci value", async () => {
+        expect(fibonacci(0)).toEqual(0);
+        expect(fibonacci(1)).toEqual(1);
+        expect(fibonacci(2)).toEqual(1);
+        expect(fibonacci(6)).toEqual(8);
+    });
+
+
+});
